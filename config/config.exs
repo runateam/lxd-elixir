734ddb4c28fe8845d95ec23c78aedb8fc7f579aa
kalaww <ceddesgranges@gmail.com>
2017-09-16 17:40:49 +0200

    rm useless cfgs

--- config/config.exs
+++ config/config.exs
@@ -3,9 +3,3 @@ use Mix.Config
 config :lxd, socket: "/var/lib/lxd/unix.socket"
 
 config :lxd, api_version: "/1.0"
-
-config :lxd, LXD.Containers,
-  all: "/containers",
-  one: "/containers/@",
-  exec: "/containers/@/exec",
-  files: "/containers/@/files"
